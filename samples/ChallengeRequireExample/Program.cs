5341abf43866cd188e2b4cac3517627462f1daa0
Ramtinak <ramtinak@live.com>
2018-09-07 19:39:01 +0430

    Nothing

--- samples/ChallengeRequireExample/Program.cs
+++ samples/ChallengeRequireExample/Program.cs
@@ -8,9 +8,6 @@ namespace ChallengeRequireExample
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
