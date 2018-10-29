53130d70485466f2cfd10badc2174a2ff5e1c20a
Ramtinak <Ramtinak@live.com>
2018-10-29 16:33:56 +0330

    Add ActionBlocked property to ResultInfo

--- src/InstagramApiSharp/Classes/ResultInfo.cs
+++ src/InstagramApiSharp/Classes/ResultInfo.cs
@@ -7,10 +7,7 @@ namespace InstagramApiSharp.Classes
         public ResultInfo(string message)
         {
             Message = message;
-            if (message.Contains("task was canceled"))
-                Timeout = true;
-            if (message.ToLower().Contains("challenge"))
-                NeedsChallenge = true;
+            HandleMessages(message);
         }
 
         public ResultInfo(Exception exception)
@@ -18,31 +15,35 @@ namespace InstagramApiSharp.Classes
             Exception = exception;
             Message = exception?.Message;
             ResponseType = ResponseType.InternalException;
-            if (Message.Contains("task was canceled"))
-                Timeout = true;
-            if (Message.ToLower().Contains("challenge"))
-                NeedsChallenge = true;
+            HandleMessages(Message);
         }
 
         public ResultInfo(ResponseType responseType, string errorMessage)
         {
             ResponseType = responseType;
             Message = errorMessage;
+            HandleMessages(errorMessage);
+        }
+        public void HandleMessages(string errorMessage)
+        {
             if (errorMessage.Contains("task was canceled"))
                 Timeout = true;
             if (errorMessage.ToLower().Contains("challenge"))
                 NeedsChallenge = true;
+            if (errorMessage.ToLower().Contains("wait a few minutes before you try again"))
+                ActionBlocked = true;
         }
-
         public Exception Exception { get; }
 
         public string Message { get; }
 
         public ResponseType ResponseType { get; }
 
-        public bool Timeout { get; }
+        public bool Timeout { get; internal set; }
 
-        public bool NeedsChallenge { get; }
+        public bool NeedsChallenge { get; internal set; }
+
+        public bool ActionBlocked { get; internal set; }
 
         public override string ToString()
         {
