00ad6b8cf2c3031e54ae52e861e1704c6f82b30e
Ramtinak <Ramtinak@live.com>
2018-11-22 00:51:19 +0330

    Result is updated with more error support

--- src/InstagramApiSharp/Classes/ResponseWrappers/Errors/BadStatusErrorsResponseRecovery.cs
+++ src/InstagramApiSharp/Classes/ResponseWrappers/Errors/BadStatusErrorsResponseRecovery.cs
@@ -6,7 +6,7 @@ namespace InstagramApiSharp.Classes.ResponseWrappers
 {
     public class BadStatusErrorsResponseRecovery : BaseStatusResponse
     {
-        [JsonProperty("errors")] public MessageErrorsResponsePhone Phone_number { get; set; }
+        [JsonProperty("errors")] public MessageErrorsResponsePhone PhoneNumber { get; set; }
     }
 
     public class MessageErrorsResponsePhone
