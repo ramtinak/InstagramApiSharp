f1c1fdf688c5adb72ff61efaa670d30463d8bf95
Ramtinak <Ramtinak@live.com>
2018-12-14 02:39:28 +0330

    Update UploadPhoto sample with progress

--- samples/Examples/Samples/UploadPhoto.cs
+++ samples/Examples/Samples/UploadPhoto.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Threading.Tasks;
 using InstagramApiSharp.API;
+using InstagramApiSharp.Classes;
 using InstagramApiSharp.Classes.Models;
 /////////////////////////////////////////////////////////////////////
 ////////////////////// IMPORTANT NOTE ///////////////////////////////
@@ -40,6 +41,35 @@ namespace Examples.Samples
             Console.WriteLine(result.Succeeded
                 ? $"Media created: {result.Value.Pk}, {result.Value.Caption}"
                 : $"Unable to upload photo: {result.Info.Message}");
+        }
+
+        public async Task DoShowWithProgress()
+        {
+            var mediaImage = new InstaImageUpload
+            {
+                // leave zero, if you don't know how height and width is it.
+                Height = 1080,
+                Width = 1080,
+                Uri = @"c:\someawesomepicture.jpg"
+            };
+            // Add user tag (tag people)
+            mediaImage.UserTags.Add(new InstaUserTagUpload
+            {
+                Username = "rmt4006",
+                X = 0.5,
+                Y = 0.5
+            });
+            // Upload photo with progress
+            var result = await _instaApi.MediaProcessor.UploadPhotoAsync(UploadProgress, mediaImage, "someawesomepicture");
+            Console.WriteLine(result.Succeeded
+                ? $"Media created: {result.Value.Pk}, {result.Value.Caption}"
+                : $"Unable to upload photo: {result.Info.Message}");
+        }
+        void UploadProgress(InstaUploaderProgress progress)
+        {
+            if (progress == null)
+                return;
+            Console.WriteLine($"{progress.Name} {progress.UploadState}");
         }
     }
 }
