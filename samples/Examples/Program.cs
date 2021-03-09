5a39e17f70e72c08c2e376e9500ad4f19652aac9
Aydin47 <aydin.zahedian@gmail.com>
2021-03-09 20:20:11 +0100

    added a working sample for going live and post it afterwards to IGTV

--- samples/Examples/Program.cs
+++ samples/Examples/Program.cs
@@ -17,129 +17,82 @@ namespace Examples
 {
     class Program
     {
-        /// <summary>
-        ///     Api instance (one instance per Instagram user)
-        /// </summary>
         private static IInstaApi InstaApi;
+        public static string entry = "";
+        private static UserSessionData user;
+        private static IResult<InstaLoginResult> loginRequest;
 
         static void Main(string[] args)
         {
             var result = Task.Run(MainAsync).GetAwaiter().GetResult();
             if (result)
                 return;
-            Console.ReadKey();
+            Console.ReadLine();
         }
         public static async Task<bool> MainAsync()
         {
-            try
+            Console.WriteLine("Starting demo of InstagramApiSharp project");
+
+            user = new UserSessionData
             {
-                Console.WriteLine("Starting demo of InstagramApiSharp project");
-                // create user session data and provide login details
-                var userSession = new UserSessionData
-                {
-                    UserName = "Username",
-                    Password = "Password"
-                };
-                // if you want to set custom device (user-agent) please check this:
-                // https://github.com/ramtinak/InstagramApiSharp/wiki/Set-custom-device(user-agent)
+                UserName = "your usernam",
+                Password = "your password"
+            };
 
-                var delay = RequestDelay.FromSeconds(2, 2);
-                // create new InstaApi instance using Builder
-                InstaApi = InstaApiBuilder.CreateBuilder()
-                    .SetUser(userSession)
-                    .UseLogger(new DebugLogger(LogLevel.All)) // use logger for requests and debug messages
-                    .SetRequestDelay(delay)
-                    .Build();
-                // create account
-                // to create new account please check this:
-                // https://github.com/ramtinak/InstagramApiSharp/wiki/Create-new-account
-                const string stateFile = "state.bin";
-                try
-                {
-                    if (File.Exists(stateFile))
-                    {
-                        Console.WriteLine("Loading state from file");
-                        using (var fs = File.OpenRead(stateFile))
-                        {
-                            InstaApi.LoadStateDataFromStream(fs);
-                            // in .net core or uwp apps don't use LoadStateDataFromStream
-                            // use this one:
-                            // _instaApi.LoadStateDataFromString(new StreamReader(fs).ReadToEnd());
-                            // you should pass json string as parameter to this function.
-                        }
-                    }
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e);
-                }
 
-                if (!InstaApi.IsUserAuthenticated)
-                {
-                    // login
-                    Console.WriteLine($"Logging in as {userSession.UserName}");
-                    delay.Disable();
-                    var logInResult = await InstaApi.LoginAsync();
-                    delay.Enable();
-                    if (!logInResult.Succeeded)
-                    {
-                        Console.WriteLine($"Unable to login: {logInResult.Info.Message}");
-                        return false;
-                    }
-                }
-                var state = InstaApi.GetStateDataAsStream();
-                // in .net core or uwp apps don't use GetStateDataAsStream.
-                // use this one:
-                // var state = _instaApi.GetStateDataAsString();
-                // this returns you session as json string.
-                using (var fileStream = File.Create(stateFile))
-                {
-                    state.Seek(0, SeekOrigin.Begin);
-                    state.CopyTo(fileStream);
-                }
-
-                Console.WriteLine("Press 1 to start basic demo samples");
-                Console.WriteLine("Press 2 to start upload photo demo sample");
-                Console.WriteLine("Press 3 to start comment media demo sample");
-                Console.WriteLine("Press 4 to start stories demo sample");
-                Console.WriteLine("Press 5 to start demo with saving state of API instance");
-                Console.WriteLine("Press 6 to start messaging demo sample");
-                Console.WriteLine("Press 7 to start location demo sample");
-                Console.WriteLine("Press 8 to start collections demo sample");
-                Console.WriteLine("Press 9 to start upload video demo sample");
-
-                var samplesMap = new Dictionary<ConsoleKey, IDemoSample>
-                {
-                    [ConsoleKey.D1] = new Basics(InstaApi),
-                    [ConsoleKey.D2] = new UploadPhoto(InstaApi),
-                    [ConsoleKey.D3] = new CommentMedia(InstaApi),
-                    [ConsoleKey.D4] = new Stories(InstaApi),
-                    [ConsoleKey.D5] = new SaveLoadState(InstaApi),
-                    [ConsoleKey.D6] = new Messaging(InstaApi),
-                    [ConsoleKey.D7] = new LocationSample(InstaApi),
-                    [ConsoleKey.D8] = new CollectionSample(InstaApi),
-                    [ConsoleKey.D9] = new UploadVideo(InstaApi)
-                };
-                var key = Console.ReadKey();
-                Console.WriteLine(Environment.NewLine);
-                if (samplesMap.ContainsKey(key.Key))
-                    await samplesMap[key.Key].DoShow();
-                Console.WriteLine("Done. Press esc key to exit...");
-
-                key = Console.ReadKey();
-                return key.Key == ConsoleKey.Escape;
-            }
-            catch (Exception ex)
+            do
             {
-                Console.WriteLine(ex);
+                Console.WriteLine("Trying to Loge in to " + user.UserName);
+                InstaApi = InstaApiBuilder.CreateBuilder().SetUser(user).UseLogger(new DebugLogger(LogLevel.Exceptions)).Build();
+                await InstaApi.SendRequestsBeforeLoginAsync();
+                await Task.Delay(5000);
+                loginRequest = await InstaApi.LoginAsync();
             }
-            finally
+            while (!loginRequest.Succeeded);
+            if (loginRequest.Succeeded)
             {
-                // perform that if user needs to logged out
-                // var logoutResult = Task.Run(() => _instaApi.LogoutAsync()).GetAwaiter().GetResult();
-                // if (logoutResult.Succeeded) Console.WriteLine("Logout succeed");
+                Console.WriteLine("Logged in to " + user.UserName);
+                //     username = user.UserName;
+                InstaApi.SetRequestDelay(RequestDelay.FromSeconds(1, 2));
             }
-            return false;
+            else
+            {
+                Console.WriteLine("Error" + loginRequest.Info.Message);
+            }
+
+            
+            Console.WriteLine("Press 1 to start basic demo samples");
+            Console.WriteLine("Press 2 to start upload photo demo sample");
+            Console.WriteLine("Press 3 to start comment media demo sample");
+            Console.WriteLine("Press 4 to start stories demo sample");
+            Console.WriteLine("Press 5 to start demo with saving state of API instance");
+            Console.WriteLine("Press 6 to start messaging demo sample");
+            Console.WriteLine("Press 7 to start location demo sample");
+            Console.WriteLine("Press 8 to start collections demo sample");
+            Console.WriteLine("Press 9 to start upload video demo sample");
+
+            var samplesMap = new Dictionary<ConsoleKey, IDemoSample>
+            {
+                [ConsoleKey.D1] = new Basics(InstaApi),
+                [ConsoleKey.D2] = new UploadPhoto(InstaApi),
+                [ConsoleKey.D3] = new CommentMedia(InstaApi),
+                [ConsoleKey.D4] = new Stories(InstaApi),
+                [ConsoleKey.D5] = new SaveLoadState(InstaApi),
+                [ConsoleKey.D6] = new Messaging(InstaApi),
+                [ConsoleKey.D7] = new LocationSample(InstaApi),
+                [ConsoleKey.D8] = new CollectionSample(InstaApi),
+                [ConsoleKey.D9] = new UploadVideo(InstaApi),
+                [ConsoleKey.D0] = new StartLive(InstaApi)
+            };
+            var key = Console.ReadKey();
+            Console.WriteLine(Environment.NewLine);
+            if (samplesMap.ContainsKey(key.Key))
+                await samplesMap[key.Key].DoShow();
+            Console.WriteLine("Done. Press esc key to exit...");
+
+            key = Console.ReadKey();
+            return key.Key == ConsoleKey.Escape;
+
         }
     }
 }
---
+++ samples/Examples/Samples/GoLiveAndPostLiveAfter.cs
@@ -0,0 +1,145 @@
+﻿using InstagramApiSharp.Classes.Models;
+using System;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using Examples.Samples;
+using InstagramApiSharp.API;
+
+namespace Examples.Samples
+{
+    internal class StartLive : IDemoSample
+    {
+        private readonly IInstaApi InstaApi;
+
+        public StartLive(IInstaApi instaApi)
+        {
+            InstaApi = instaApi;
+        }
+
+        private string key;
+        public async Task DoShow()
+        {
+            try
+            {
+                var during = await InstaApi.LiveProcessor.GetSuggestedBroadcastsAsync();
+                if (during.Succeeded)
+                {
+                    Console.WriteLine("Live now ID:  " + during.Value);
+                    await InstaApi.LiveProcessor.EndAsync(during.Value.ToString());
+                }
+                else
+                {
+                    var myVar = await InstaApi.LiveProcessor.CreateAsync(broadcastMessage: "Hello there :D");
+                    string BID = "";
+                    if (myVar.Succeeded)
+                    {
+                        BID = myVar.Value.BroadcastId.ToString();
+                        Console.WriteLine("BroadcastID:  " + BID);
+                        Console.WriteLine("UploadUrl:  " + myVar.Value.UploadUrl);
+                        Console.WriteLine("Go to OBS and give the pass, when y/Y the stream will start");
+                        key = Console.ReadLine();
+                        if (key == "y")
+                        {
+                            var stream = await InstaApi.LiveProcessor.StartAsync(BID, true);
+                            await InstaApi.LiveProcessor.EnableCommentsAsync(BID);
+                            while (1 == 1)
+                            {
+                                Console.WriteLine("f for finish, y for live count, c for chearing them");
+                                key = Console.ReadLine();
+                                if (key == "y")
+                                {
+                                    var viewers = await InstaApi.LiveProcessor.GetViewerListAsync(BID);
+                                    if (viewers.Succeeded)
+                                    {
+                                        var count = viewers.Value.Count;
+                                        Console.WriteLine("Count is: " + count);
+                                        viewers.Value.ForEach(x => Console.WriteLine(x.FullName));
+                                    }
+                                }
+                                if (key == "f")
+                                {
+                                    Console.WriteLine("Are you sure ? y/n");
+                                    key = Console.ReadLine();
+                                    if (key == "y")
+                                    {
+                                        var endLive = await InstaApi.LiveProcessor.EndAsync(BID);
+                                        if (endLive.Succeeded)
+                                        {
+                                            break;
+                                        }
+                                    }
+                                }
+                            }
+                            Console.WriteLine("Post the live to IGTV? y/n");
+                            key = Console.ReadLine();
+                            if (key == "y")
+                            {
+                                var cr = await InstaApi.TVProcessor.GetCreationTools();
+                                if (cr.Succeeded)
+                                {
+                                    var s = await InstaApi.LiveProcessor.GetPostLiveThumbnails(BID);
+                                    var thumbnailcount = s.Value.Thumbnails.Count;
+                                    Random rnd = new Random();
+
+                                    if (s.Succeeded)
+                                    {
+                                        while (1 == 1)
+                                        {
+                                            var thumbnailNumber = rnd.Next(1, thumbnailcount);
+                                            using (WebClient client = new WebClient())
+                                            {
+                                                client.DownloadFile(new Uri(s.Value.Thumbnails[thumbnailNumber]), @"c:\temp\image.jpg");
+                                            }
+                                            Console.WriteLine($"Accept the Thumbnail {thumbnailNumber} out of {thumbnailcount} in 'c->temp->image.jpg' for IGTV live ? y/n");
+                                            key = Console.ReadLine();
+                                            if (key == "y")
+                                            {
+                                                Console.WriteLine($"Thumbnail {thumbnailNumber} will be used.");
+                                                break;
+                                            }
+                                        }
+
+                         //               using (Image image = Image.Load(@"c:\temp\image.jpg")) 
+                         //               {
+                         //                   image.Mutate(x => x
+                         //                        .Resize(1080, 1920));
+                         //                   image.Save(@"c:\temp\fb.jpg"); // Automatic encoder selected based on extension.
+                         //               }
+
+                                        var pic = new InstaImageUpload
+                                        {
+                                            ImageBytes = File.ReadAllBytes(@"c:\temp\fb.jpg"), // here use the downloaded image or use another picture and give the path
+                                            Height = 1920,
+                                            Width = 1080,
+                                        };
+
+                                        string title = "Test tittle ";
+                                        string caption = "Test caption";
+                                        Console.WriteLine("title: " + title +  "Caption: " + caption);
+                                        Console.WriteLine("wait...");
+                                        var vv2 = await InstaApi.LiveProcessor.AddToPostLiveAsync(pic, title, caption, BID);
+                                        while (1 == 1)
+                                        {
+                                            if (vv2.Succeeded)
+                                            {
+                                                Console.WriteLine("Succesfully published");
+                                                break;
+                                            }
+                                            Console.WriteLine("IDK WHY ?!?!? Press F9 and bring the curser to line 125 -> AddToPostLiveAsync");
+                                        }
+
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
